1d9c6bf14088c3e03a12391e121861bfb059edcc
Maulana Achmad 042 <Achmadaminullah546@gmail.com>
2023-01-11 23:59:37 +0700

    controller

--- resources/views/admin/room/room.blade.php
+++ resources/views/admin/room/room.blade.php
@@ -4,26 +4,30 @@
 @endsection
 @section('content')
 <div class="card mb-3 p-5">
-    
-    <h3 class="card-title">Room</h3>
+
+    <h3 class="card-title">Room</h3>  
     <div class="row g-0 position-relative">
+      @foreach ($datas as $room)
+          
       <div class="col-md-4">
-        <img src="{{ asset('img/krte.jpg') }}" class="img-fluid rounded-start" alt="...">
+        <img src="{{ $room->image }}" class="img-fluid rounded-start" alt="Gambar" >
       </div>
       <div class="col-md-4">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">{{$room->name}}</h5>
+          <p class="card-text">{{$room->capacity}}</p>
           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
       </div>
       <div class="col-md-4 position-absolute" style="bottom: 20px; right:0px">
         <a href="{{ URL::to('admin/room/editfacility') }}" class="btn btn-warning">Edit Facility</a>
       </div>
+      @endforeach
     </div>
+    
   </div>
   <div class="w-100 text-center">
-    <a class="btn btn-primary mt-5" href="{{ URL::to('admin/room/addroom') }}">Add Room</a>
+    <a class="btn btn-primary mt-5" href="{{ route('addroom') }}">Add Room</a>
   </div>
 @endsection
 @show
