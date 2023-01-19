3e8b1fe4040d2097036a190a4953d46c743ba83d
Akhmad Khasan Abdullah <41146877+akhmadkhasan68@users.noreply.github.com>
2023-01-19 14:36:28 +0700

    add route defaul

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\ApiResponseHelper;
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Booking\BookingController;
 use App\Http\Controllers\Api\Divisions\DivisionController;
@@ -19,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/', function(){
+    return ApiResponseHelper::successResponse("Hello from booking apps API", []);
+});
 
 Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::group(['prefix' => 'auth'], function() {
