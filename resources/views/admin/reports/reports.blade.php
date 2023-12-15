33a96c842643772b6bdfeffe28191f14a79739fa
SulthoniAkbar <muhammadsultoni71@gmail.com>
2023-12-15 15:59:52 +0700

    Fix tabel report

--- resources/views/admin/reports/reports.blade.php
+++ resources/views/admin/reports/reports.blade.php
@@ -3,34 +3,34 @@
     
 @endsection
 @section('content')
-<div class="card card-primary">
-    <div class="card-body">
+    <div class="card card-primary">
+        <div class="card-body">
 
-        <h3 class="card-title">Report</h3>
+            <h3 class="card-title">Report</h3>
 
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-            <thead class="btn-primary">
-                <tr>
-                    <th>Name</th>
-                    <th>Nama Ruangan</th>
-                    <th>Deskripsi</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead class="btn-primary">
+                    <tr>
+                        <th>Name</th>
+                        <th>Nama Ruangan</th>
+                        <th>Deskripsi</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
                     @foreach ($datas as $index)
-                        <td>{{ $index->member->name }}</td>
-                        <td>{{ $index->room->name }}</td>
-                        <td>{{ $index->description }}</td>
-                        <td>
-                            <a href="{{ route('detailreport', $index->id) }}" class="btn btn-info">Detail</a>
-                        </td>
+                        <tr>
+                            <td>{{ $index->member->name }}</td>
+                            <td>{{ $index->room->name }}</td>
+                            <td>{{ $index->description }}</td>
+                            <td>
+                                <a href="{{ route('detailreport', $index->id) }}" class="btn btn-info">Detail</a>
+                            </td>
+                        </tr>
                     @endforeach
-                </tr>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
 @show
