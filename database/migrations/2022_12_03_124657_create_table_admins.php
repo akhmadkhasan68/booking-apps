9030e3285a3037afdc04577ede48eecd82a9d44a
Akhmad Khasan Abdullah <41146877+akhmadkhasan68@users.noreply.github.com>
2024-02-29 23:10:37 +0700

    fix: migration

--- database/migrations/2022_12_03_124657_create_table_admins.php
+++ database/migrations/2022_12_03_124657_create_table_admins.php
@@ -32,6 +32,6 @@ class CreateTableAdmins extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_admins');
+        Schema::dropIfExists('admins');
     }
 }
